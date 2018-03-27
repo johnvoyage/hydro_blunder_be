d7b4685ff3c921ac22277a6aeed1e0a2f397d6ba
johnvoyage <jmyork762@gmail.com>
2018-03-27 14:39:50 -0400

    a lot be

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,45 @@
 class Api::V1::UsersController < ApplicationController
 
   def index
-      @users = User.all
-      render json: @users
-    end
+    @users = User.all
+    render json: @users
+  end
 
-    def update
-      @user = User.find(params[:id])
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
 
-      @user.update(note_params)
-      if @user.save
-        render json: @user
-      else
-        render json: {errors: @user.errors.full_messages}, status: 422
-      end
-    end
+  def new
+    @user = User.new
+  end
 
-    private
-    def note_params
-      params.require(:user).permit(:username, :high_score, :game_count, :drink_count)
+  def create
+    @user = User.find_or_create_by(user_params)
+    render json: @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    @user.update(note_params)
+    if @user.save
+      render json: @user
+    else
+      render json: {errors: @user.errors.full_messages}, status: 422
     end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.delete
+    render json: @users
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :high_score, :game_count, :drink_count)
+  end
 
 end
